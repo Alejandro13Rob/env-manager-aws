a1ba1320c1326358f0ba9f65a091d5e2632c72b4
Roberto Alejandro Mendoza Gallegos <roberto.mendoza@wizeline.com>
2021-08-18 18:12:13 -0500

    1.0.0

---
+++ src/cli.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// Grab provided args
+import { configure } from './configure';
+import { fileExists } from './utils';
+const Path = require('path');
+
+const [, , ...args] = process.argv;
+
+switch (args[0]) {
+    case 'configure':
+        configure(args.slice(1)).then(() => process.exit(0));
+        break;
+    default:
+        process.exit(0);
+        break;
+}
